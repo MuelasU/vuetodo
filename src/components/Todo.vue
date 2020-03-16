dd82ba0144fb4630a062fc003f4d12936b4c64c6
GMuelas <bimuelas@gmail.com>
2020-03-15 22:51:01 -0300

    Primary To-do

---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="todo">
+    <input type="text" placeholder="I have to ..." v-model="title" @keyup.enter="AddItem" />
+    <ul>
+      <todo-item v-for="todo in todos" v-bind:key="todo" :title="todo"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      title: "",
+      todos: ["Pratice more Vue.js"]
+    };
+  },
+  methods: {
+    AddItem() {
+      this.todos.push(this.title);
+      this.title = "";
+    }
+  },
+  components: {
+    "todo-item": require("./Todo_Item").default
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
---
+++ src/components/Todo_Item.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo-item">
+    <li>{{title}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
