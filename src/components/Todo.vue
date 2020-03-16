ead70c384bcf6e0d7ca1f7a701caae2d9f12102c
GMuelas <bimuelas@gmail.com>
2020-03-16 16:41:39 -0300

    Items go up and down

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,12 +2,21 @@
   <div class="todo">
     <input type="text" placeholder="I have to ..." v-model="title" @keyup.enter="AddItem" />
     <ul>
-      <todo-item v-for="todo in todos" v-bind:key="todo" :title="todo"></todo-item>
+      <todo-item
+        v-for="(todo, index) in todos"
+        v-bind:key="index"
+        v-bind:title="todo"
+        v-bind:index="index"
+        v-on:itemUp="ItemGoUp"
+        v-on:itemDown="ItemGoDown"
+      ></todo-item>
     </ul>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   name: "Todo",
   data() {
@@ -20,6 +29,22 @@ export default {
     AddItem() {
       this.todos.push(this.title);
       this.title = "";
+    },
+    ItemGoUp(index) {
+      if (index !== 0) {
+        var deCima = this.todos[index - 1];
+        var esse = this.todos[index];
+        Vue.set(this.todos, index, deCima);
+        Vue.set(this.todos, index - 1, esse);
+      }
+    },
+    ItemGoDown(index) {
+      if (index !== this.todos.length - 1) {
+        var deBaixo = this.todos[index + 1];
+        var esse = this.todos[index];
+        Vue.set(this.todos, index, deBaixo);
+        Vue.set(this.todos, index + 1, esse);
+      }
     }
   },
   components: {
--- src/components/Todo_Item.vue
+++ src/components/Todo_Item.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="todo-item">
-    <li>{{title}}</li>
+    <li>
+      <span>{{title}}</span>
+      <i class="material-icons" @click="GoUp">keyboard_arrow_up</i>
+      <i class="material-icons" @click="GoDown">keyboard_arrow_down</i>
+    </li>
   </div>
 </template>
 
@@ -8,10 +12,22 @@
 export default {
   name: "TodoItem",
   props: {
-    title: String
+    title: String,
+    index: Number
+  },
+  methods: {
+    GoUp() {
+      this.$emit("itemUp", this.index);
+    },
+    GoDown() {
+      this.$emit("itemDown", this.index);
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+i {
+  cursor: pointer;
+}
 </style>
